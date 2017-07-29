1b83a2393edf914440d6a5dce976afea6b129ae5
Artem Umanets <artem.u.v@gmail.com>
2017-07-29 18:52:28 +0100

    Removed unnecessary resources and added localization file

---
+++ BRA/classes/ViewControllers/LoadingScreenViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoadingScreenViewController.swift
+//  BRA
+//
+//  Created by Artem Umanets on 28/07/2017.
+//  Copyright © 2017 Seedrop. All rights reserved.
+//
+
+import UIKit
+
+class LoadingScreenViewController: UIViewController {
+    @IBOutlet weak var activityLoading: UIActivityIndicatorView!
+    
+    @IBOutlet weak var viewRetryWidget: UIView!
+    @IBOutlet weak var labelStatusInfo: UILabel!
+    @IBOutlet weak var buttonRetry: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.labelStatusInfo.text = loc("LoadingScreen.ServerStatusNotOK")
+        self.buttonRetry.setTitle(loc("LoadingScreen.Retry"), for: .normal)
+        
+        self.viewRetryWidget.alpha = 0.0
+        self.validateServerHealth()
+    }
+
+    func validateServerHealth(){
+        let healthStatusOperation = OperationGetHealthStatus()
+        healthStatusOperation.performOperation { (status:StatusModel?, error:Error?) in
+            guard error == nil else{
+                print("An error occurred processing request: \(error!.localizedDescription)")
+                UIView.animate(withDuration: kAnimationSpeed, animations: { 
+                    self.viewRetryWidget.alpha = 1.0
+                    self.activityLoading.alpha = 0.0
+                })
+                return
+            }
+            
+            // navigate to next screen
+            status?.printDescription()
+        }
+    }
+    
+    // MARK: Actions
+    @IBAction func retryTapped(_ sender: Any) {
+        UIView.animate(withDuration: kAnimationSpeed, animations: { 
+            self.viewRetryWidget.alpha = 0.0
+            self.activityLoading.alpha = 1.0
+        }) { (completed) in
+            self.validateServerHealth()
+        }
+    }
+}
