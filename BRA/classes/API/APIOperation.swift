4ccea3f6e4baf2b1ec3836f4b807b280469d1f29
Artem Umanets <artem.u.v@gmail.com>
2017-07-29 18:00:44 +0100

    Improved implementation of the API communication routines Generalized implementation of the GET and POST methods

--- BRA/classes/API/APIOperation.swift
+++ BRA/classes/API/APIOperation.swift
@@ -1,5 +1,5 @@
 //
-//  APIHandler.swift
+//  APIOperation.swift
 //  BRA
 //
 //  Created by Artem Umanets on 28/07/2017.
@@ -9,8 +9,7 @@
 import Foundation
 import SwiftyJSON
 
-
-typealias CompletionBlock = (_ result:AnyObject?, _ error: Error?) -> Void
+typealias CompletionBlock = (_ result:Any?, _ error: Error?) -> Void
 
 class APIOperation{
     let request : APIRequestModel
@@ -20,8 +19,9 @@ class APIOperation{
     }
     
     internal func makeRequest(completion:@escaping CompletionBlock){
-        var urlRequest = URLRequest(url: self.request.requestURL)
-        urlRequest.httpMethod = self.request.method.rawValue
+        //var urlRequest = URLRequest(url: self.request.requestURL)
+        //urlRequest.httpMethod = self.request.method.rawValue
+        var urlRequest = self.request.getRequest()
         let session = URLSession.shared
 
         let task = session.dataTask(with: urlRequest) { data, response, error in
@@ -36,7 +36,6 @@ class APIOperation{
                 return
             }
             
-            //do {
             let json = JSON(data: responseData)
             guard json.error == nil else {
                 let erroParsingJSON = NSError(domain:kErrorDomain, code:kErrorNetworkParsingJSON, userInfo:[NSLocalizedDescriptionKey:"Could not parse json response.", NSLocalizedFailureReasonErrorKey : json.error!.localizedDescription])
@@ -45,36 +44,18 @@ class APIOperation{
             }
             let responseModel = self.createModel(data: json)
             completion(responseModel, nil)
-            
-                /*
-                guard let todo1 = try JSONSerialization.jsonObject(with: responseData, options: [])
-                    as? [Any] else {
-                        print("error trying to convert data to JSON")
-                        return
-                }
-                
-                guard let todo = try JSONSerialization.jsonObject(with: responseData, options: [])
-                    as? [String: Any] else {
-                        print("error trying to convert data to JSON")
-                        return
-                }
-
-                print("The todo is: " + todo.description)
-                guard let todoTitle = todo["title"] as? String else {
-                    print("Could not get todo title from JSON")
-                    return
-                }
-                print("The title is: " + todoTitle)*/
-            /*} catch  {
-                print("error trying to convert data to JSON")
-                return
-            }*/
         }
         task.resume()
     }
     
+    internal func performOperation<T : Any>(completion:@escaping (_ result:T?, _ error: Error?) -> Void) {
+        self.makeRequest { (result, error) in
+            completion(result as? T, error)
+        }
+    }
+    
     // Hook methods
-    internal func createModel(data:JSON?) -> AnyObject{
+    internal func createModel(data:JSON) -> Any{
         preconditionFailure("Unimplemented Method: This method needs to be overriden by a concrete subscass")
     }
 }
--- BRA/classes/API/APIRequestModel.swift
+++ BRA/classes/API/APIRequestModel.swift
@@ -1,5 +1,5 @@
 //
-//  APIRequest.swift
+//  APIRequestModel.swift
 //  BRA
 //
 //  Created by Artem Umanets on 29/07/2017.
@@ -20,29 +20,75 @@ enum OperationMethod : String{
 
 class APIRequestModel{
     let operation : String
+    let argumentAsSubpath : Bool
     let method : OperationMethod
     private var arguments : [APIArgument] = [APIArgument]()
     
-    var requestURL : URL{
-        get{
-            var arguments = ""
-            if self.arguments.count > 0{
-                arguments = "?"
-                for var i in 0..<self.arguments.count{
-                    arguments += self.arguments[i].value
+    func getRequest() -> URLRequest{
+        var urlRequest : URLRequest!
+        var url = "\(APIEndpointURL)\(self.operation)"
+        
+        if self.method == .get{
+            url.append(self.getArgumentsList())
+            urlRequest = URLRequest(url: URL(string: url)!)
+        }else if self.method == .post{
+            urlRequest = URLRequest(url: URL(string: url)!)
+            urlRequest.httpBody = self.getArgumentsList().data(using: String.Encoding.utf8)
+        }
+        urlRequest.httpMethod = self.method.rawValue
+        return urlRequest
+    }
+    
+    private func getArgumentsList() -> String{
+        var arguments = ""
+        if self.arguments.count > 0{
+            if self.method == .get{
+                if self.argumentAsSubpath && self.arguments.count == 1{
+                    arguments = "/\(self.arguments[0].value)"
+                }else{
+                    arguments = "?"
+                    for i in 0..<self.arguments.count{
+                        arguments += self.arguments[i].value
+                        if i < self.arguments.count - 1{
+                            arguments += "&"
+                        }
+                    }
+                }
+            }else{
+                for i in 0..<self.arguments.count{
+                    arguments += "\(self.arguments[i].name)=\(self.arguments[i].value)"
                     if i < self.arguments.count - 1{
                         arguments += "&"
                     }
                 }
             }
-            let finalURL = URL(string:"\(APIEndpointURL)\(self.operation)\(arguments)")!
-            print("URL:", finalURL)
-            return finalURL
+            
+        }
+        return arguments
+    }
+    
+    /*private var requestURLforMethodPOST : URL{
+        get{
+            var url = "\(APIEndpointURL)\(self.operation)"
+            return URL(string: url)
         }
     }
     
-    init(operation: String, method:OperationMethod = .get) {
+    private var requestURLforMethodGET : URL{
+        get{
+            var url = "\(APIEndpointURL)\(self.operation)"
+            if self.method == .get && self.argumentAsSubpath && self.arguments.count == 1{
+                url.append("/\(self.arguments[0].value)")
+            }else{
+                url.append(self.getArgumentsList())
+            }
+            return URL(string: url)!
+        }
+    }*/
+    
+    init(operation: String, argumentAsSubpath:Bool = false, method:OperationMethod = .get) {
         self.operation = operation
+        self.argumentAsSubpath = argumentAsSubpath
         self.method = method
     }
     
